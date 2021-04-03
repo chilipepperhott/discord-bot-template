7ea336f4421194ddd05415b04a7a2a13384676c1
Elijah <elijah.sirius@protonmail.com>
2021-04-02 23:06:43 -0700

    Init

---
+++ src/bot.rs
@@ -0,0 +1,52 @@
+use serenity::framework::standard::{
+    help_commands, macros::help, Args, CommandGroup, CommandResult, HelpOptions, StandardFramework,
+};
+pub struct Bot;
+
+impl Bot{
+    pub fn run(token: &str) -> Result<!>{
+        // Create the standard framework
+        let framework = StandardFramework::new()
+            .configure(|c| {
+                c.prefix("{{bot_prefix}}").case_insensitivity(true);
+            })
+            // Add each command group here using .group([GROUP NAME IN CAPS])
+            {% if include_examples %}
+            .group(&PONG)
+            {% endif %}
+            .help(&HELP);
+
+        // Create and start the client
+        let mut client = Client::builder(token).framework(framework).await?;
+        client.start().await
+    }
+}
+
+#[help]
+/// Generates help text for the bot
+async fn help(
+    context: &Context,
+    msg: &Message,
+    args: Args,
+    help_options: &'static HelpOptions,
+    groups: &[&'static CommandGroup],
+    owners: HashSet<UserId>,
+) -> CommandResult {
+    let _ = help_commands::with_embeds(context, msg, args, help_options, groups, owners).await;
+    Ok(())
+}
+
+{% if include_examples %}
+
+// This is an example group. You should put actual groups into their own seperate files
+#[group]
+#[commands(pong)]
+struct Pong;
+
+#[command("ping")]
+async fn pong(ctx: &Context, msg: &Message) -> CommandResult{
+    msg.channel_id.broadcast_typeing(ctx).await?;
+    msg.reply(ctx, "Pong!");
+}
+
+{% endif %}
---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::io::Read;
+use bot::Bot;
+
+mod bot;
+
+fn err_msg(message: &str) {
+    println!("{}. Press any key to exit", message);
+            std::io::stdin().read_line(&mut String::new()).unwrap();
+}
+
+#[tokio::main]
+fn main() {
+    // We also want to option to store the token in a `.env` file
+    dotenv::dotenv().ok();
+
+    // Grab the bot token from the environment
+    let token;
+    match env::var("{{token_variable_name}}"){
+        Ok(t) => token = t,
+        Err(_) => {
+            err_msg("Could not find bot token in environment");
+            return;
+        }
+    }
+
+    // Run the bot. If it errors out, let the user know.
+    if let Err(err) = Bot::run(token).await{
+        err_msg("The bot encountered a catastrophic error. Please contact the bot creator if this happens frequently");
+        return;
+    }
+}

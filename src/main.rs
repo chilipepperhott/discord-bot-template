cd2006f98433b864bf168fcda1b59bc724e66b94
Elijah <elijah.sirius@protonmail.com>
2021-04-03 13:02:19 -0700

    Now uses expect rather than wacky stuff

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,13 @@ use bot::Bot;
 
 mod bot;
 
-fn err_msg(message: &str) {
-    println!("{}. Press any key to exit", message);
-            std::io::stdin().read_line(&mut String::new()).unwrap();
-}
-
 #[tokio::main]
 async fn main() {
     // We also want to option to store the token in a `.env` file
     dotenv::dotenv().ok();
 
     // Grab the bot token from the environment
-    let token;
-    match env::var("{{token_variable_name}}"){
-        Ok(t) => token = t,
-        Err(_) => {
-            err_msg("Could not find bot token in environment");
-            return;
-        }
-    }
+    let token = env::var("{{token_variable_name}}").expect("Could not load token from environment");
 
     // Run the bot. If it errors out, let the user know.
     if let Err(_) = Bot::run(token.as_str()).await{
